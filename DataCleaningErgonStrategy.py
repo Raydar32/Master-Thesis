89d25bb760797ac3bf8f25bd96f97a6b8ce8c96c
Alessandro <alessandro.mini96@libero.it>
2022-03-31 19:54:16 +0200

    Tested script for palo alto dump and cleaning

--- DataCleaningErgonStrategy.py
+++ DataCleaningErgonStrategy.py
@@ -11,7 +11,7 @@ apply, such as time-delta and protocols to exclude.
 import pandas as pd 
 
 #List of protocols to exclude 
-exclusion = ["ms-product-activation",
+application_exclusions = ["ms-product-activation",
             "ms-update",
             "incomplete",
             "ms-ds-smbv3",
@@ -27,14 +27,19 @@ exclusion = ["ms-product-activation",
             "ntp",
             "ping"]
 
+#"G:\\30d_traffic_ergon.csv"
+df = pd.read_csv("G:\\30d_traffic_ergon.csv")
 
-df = pd.read_csv("C:\\Users\\Alessandro Mini\\Downloads\\30d_traffic_ergon.csv")
 
-
+#Removing corrupted traffic
+df = df[~df.src_port.eq(0)]
+df = df[~df.dst_port.eq(0)]
+df = df[~df.bytes_sent.eq(0)]
+df = df[~df.bytes_recieved.eq(0)]
 #Applying exclusion rules
 print("Applying protocols exclusion rules")
 len_before = len(df)
-df = df[~df['application'].isin(exclusion)]
+df = df[~df['application'].isin(application_exclusions)]
 len_after = len(df)
 print("----------------------------------------------------")
 print("Bef: " , len_before , " after: ", len_after)
@@ -42,54 +47,50 @@ print("Removed data : " ,len_before/len_after)
 print("----------------------------------------------------")
 
 
-#In questo punto bisogna inserire la rimozione dei record
-#in orario notturno.
 
-#df["timestamp"].between_time('18:00', '8:00')
 print("Removing night traffic from 18:00 to 8:00")
 df["timestamp"] = pd.to_datetime(df["timestamp"])
 df = df.set_index('timestamp')
 night = df.between_time('18:00', '8:00')
-df = df.drop(night.index,inplace = True)
-df.reset_index()
+df = df.drop(night.index)
+df = df.reset_index()
+len_after = len(df)
+print("----------------------------------------------------")
+print("Bef: " , len_before , " after: ", len_after)
+print("Removed data : " ,len_before/len_after)
+print("----------------------------------------------------")
 
 
 
 
-
-k = 10
-print("Removing sources with less than " , k , " hours of traffic ")
-#Find sources with less than k hours of traffic
+k = 2
+print("Identifying sources with less than " , k , " hours of traffic ")
 
 df["timestamp"] = pd.to_datetime(df["timestamp"])
 grouped = df.groupby(pd.Grouper(freq="1H", key="timestamp"))
-#keys_uniques = grouped.groups.keys()
-#ora = grouped.get_group("2022-03-18 17:00:00+0100")
-#Per tutti gli indirizzi ip, scorri tutte le ore
-#Se un indirizzo compare  in > 3 risparmialo e salvalo, altrimenti 
-#eliminalo (in questa prima fase, perchè poi magari si butta in un
-#cluster, oppure si costruisce una tabella)
-
-
 i = 0
 sufficient = []
 insufficient = []
 for ip in df["src_ip"].unique():
-    print ("working ", ip ," ",i, " di ", len(df["src_ip"].unique()))
+    #print ("working ", ip ," ",i, " di ", len(df["src_ip"].unique()))
     found = 0
     i = i + 1 
-    for hour in grouped.groups.keys():        
-    
-        a = grouped.get_group(hour)
-        a = a.loc[a["src_ip"]==ip]
-        if(len(a)>1):      
-            found = found + 1                
+    for hour in grouped.groups.keys():            
+        try:
+            a = grouped.get_group(hour)
+            a = a.loc[a["src_ip"]==ip]
+        except:
+            found = 0 
+        else:
+            if(len(a)>1):      
+                found = found + 1                
     if found >= k:
         sufficient.append(ip)              
     else:
         insufficient.append(ip)
     found = 0                          
       
+    
 print("Removing IPs with less then ", k , " hours of traffic ")
 df = df[~df['src_ip'].isin(insufficient)]
 len_after = len(df)
@@ -97,19 +98,19 @@ print("----------------------------------------------------")
 print("Bef: " , len_before , " after: ", len_after)
 print("Removed data : " ,len_before/len_after)
 print("----------------------------------------------------")
-
+df = df.sort_values(by='timestamp', ascending=True)
 
 
 
 #Creare un nuovo CSV: Dataset_refined.csv
             
 
-#Print della quantità di record per IP univoco
-#for label in df["src_ip"].unique():
-#    print(label, "  ", len(df.loc[df["src_ip"]==label]))
+#Temporary
+for label in sufficient:
+    print(label, "  ", len(df.loc[df["src_ip"]==label]))
     
     
-    
+df.to_csv("dataset_refined.csv",index= False)
     
     
     
