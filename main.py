8d133f2618045b82c39d642b5ba969c7bdbd27b0
Alessandro <alessandro.mini96@libero.it>
2022-03-31 22:57:50 +0200

    Minor update

--- main.py
+++ main.py
@@ -6,10 +6,16 @@ Test file to keep everything together
 
 
 from ErgonDatasetCleaner import ErgonDataCleaner
+import pandas as pd 
+#------------------- Pulisco il dataset -------------------
 DataC = ErgonDataCleaner()
 DataC.setVerbose(True)
-DataC.loadDataset("G:\\basic_dataset.csv")
+DataC.loadDataset("G:\\30d_traffic_ergon.csv")
 DataC.cleanDataset()
 DataC.setOutput("G:\\refined.csv")
 print("Ratio: ", DataC.getRatio())
 
+#Apro il dataset
+df = pd.read_csv("G:\\refined.csv")
+
+#b = df.loc[df["src_ip"].str.contains("192.168.120")]
