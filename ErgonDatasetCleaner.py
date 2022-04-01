11123bff4dd74a16c3b7ff4fd9959f74c4812c93
Alessandro <alessandro.mini96@libero.it>
2022-04-01 16:58:47 +0200

    Solved bugs

--- ErgonDatasetCleaner.py
+++ ErgonDatasetCleaner.py
@@ -7,7 +7,7 @@ Created on Thu Mar 31 22:06:29 2022
 
 import pandas as pd 
 from DataCleanerInterface import DataCleaner
-
+from datetime import timedelta 
 class ErgonDataCleaner(DataCleaner):                
     def cleanDataset(self):
         application_exclusions = ["ms-product-activation",
@@ -41,36 +41,34 @@ class ErgonDataCleaner(DataCleaner):
         self.df["timestamp"] = pd.to_datetime(self.df["timestamp"],utc=True)
         self.df = self.df.set_index('timestamp')
         night = self.df.between_time('18:00', '8:00')
-        self.df = [~self.df.index.isin(night.index)]
+        self.df = self.df[~self.df.index.isin(night.index)]
         self.df = self.df.reset_index()        
-        k = 2
+        k = 3
         
         #Qui ci sono problemi, tronca il mio ip 
-        self.vprint("Identifying sources with less than " , k , " hours of traffic ")
+        self.vprint("Identifying sources with more then " , k , " hours of traffic ")
         self.df["timestamp"] = pd.to_datetime(self.df["timestamp"],utc=True)
+        
+        #q = self.df.loc[self.df["src_ip"]=="192.168.121.47"]["timestamp"].iat[-1] - self.df.loc[self.df["src_ip"]=="192.168.121.47"]["timestamp"].iat[0] 
+        
         grouped = self.df.groupby(pd.Grouper(freq="1H", key="timestamp"))
         i = 0
         sufficient = []
         insufficient = []
         for ip in self.df["src_ip"].unique():
-            #print ("working ", ip ," ",i, " di ", len(df["src_ip"].unique()))
-            found = 0
-            i = i + 1 
-            for hour in grouped.groups.keys():
-        
-                try:
-                    a = grouped.get_group(hour)
-                    a = a.loc[a["src_ip"]==ip]
-                except:
-                    found = 0 
-                else:
-                    if(len(a)>1):      
-                        found = found + 1                
-            if found >= k:
-                sufficient.append(ip)              
+           
+            delta = self.df.loc[self.df["src_ip"]==ip]["timestamp"].iat[-1] - self.df.loc[self.df["src_ip"]==ip]["timestamp"].iat[0] 
+            
+            dbgflag = False
+            if delta > timedelta(hours=k):
+                sufficient.append(ip)
+                dbgflag = True
             else:
                 insufficient.append(ip)
-            found = 0            
+            print(ip, " ",dbgflag)
+                
+    
+                
         self.df = self.df[~self.df['src_ip'].isin(insufficient)]
         self.df = self.df.sort_values(by='timestamp', ascending=True)
         
