242388672edda37f5a6572430d1083b094714669
Alessandro <alessandro.mini96@libero.it>
2022-04-06 22:52:42 +0200

    Update

--- main.py
+++ main.py
@@ -14,18 +14,19 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from kneed import KneeLocator
 from KMeansClustering import KMeansClustering
-
+from scipy import stats
+from sklearn.preprocessing import MaxAbsScaler
 #------------------- Pulisco il dataset -------------------
 DataC = ErgonDataCleaner()
 DataC.setVerbose(True)
-DataC.loadDataset("C:\\InProgress\\Tesi\\aprile.csv")
-DataC.cleanDataset()
-DataC.setOutput("C:\\InProgress\\Tesi\\aprile_r.csv")
+DataC.loadDataset("C:\\Users\\Alessandro\\Downloads\\aprile_r.csv")
+#DataC.cleanDataset()
+DataC.setOutput("C:\\Users\\Alessandro\\Downloads\\aprile_r.csv")
 
 print("Ratio: ", DataC.getRatio())
 
 
-df = pd.read_csv("C:\\InProgress\\Tesi\\aprile_r.csv")
+df = pd.read_csv("C:\\Users\\Alessandro\\Downloads\\aprile_r.csv")
 
 
 
@@ -63,18 +64,20 @@ for hour in grouped.groups.keys():
     print("Scanning time-slot :",i," di ", len(grouped.groups.keys()),hour.hour, ":",hour.minute," giorno ", hour.day, "//",
           hour.month, " totale ", len(grouped.groups.keys()), esito)
 
-extracted = normalize(extracted)
-
+#extracted = normalize(extracted)
+extracted_values = MaxAbsScaler().fit_transform(extracted)
+extracted = pd.DataFrame(extracted_values,index=extracted.index,columns=extracted.columns)
+extracted=extracted[(np.abs(stats.zscore(extracted)) < 3).all(axis=1)]
 
 #----------- Test: rimozione featues non rilevanti -------------
-del extracted["http_ratio"]
-del extracted["dst_ip"]
-del extracted["bytes"]
-del extracted["packets"]
-del extracted["dst_port"]
-del extracted["ssh_ratio"]
-del extracted["avg_duration"]
-del extracted["smtp_ratio"]
+#+del extracted["http_ratio"]
+#del extracted["dst_ip"]
+#del extracted["bytes"]
+#del extracted["packets"]
+#del extracted["dst_port"]
+#del extracted["ssh_ratio"]
+#del extracted["avg_duration"]
+#del extracted["smtp_ratio"]
 
 
 
@@ -97,41 +100,41 @@ print("Clustering k-means: " , cnum, " ",score)
 #mio = extracted.loc[extracted.index=="192.168.121.47"]
 
 
-#---------------------------------- DBSCAN POC -------------------------------------------------
-
-
-from sklearn.neighbors import NearestNeighbors
-from matplotlib import pyplot as plt
-from sklearn.cluster import DBSCAN
+#---------------------------------- Spectral  POC -------------------------------------------------
 
 
 
+#Clustering spettrale
+from sklearn.metrics import davies_bouldin_score
+from sklearn.cluster import SpectralClustering
+clustering = SpectralClustering(assign_labels='discretize').fit(extracted)
+labels_spectral = clustering.labels_
+print("SC spectral: ", silhouette_score(extracted, labels_spectral,metric="euclidean"))
+print("DB spectral: ", davies_bouldin_score(extracted, labels_spectral))
 
-scores = {0:0}
-for j in range(1,15):    
-    nn = extracted.shape[1]*j
-    nbrs = NearestNeighbors(n_neighbors=nn, algorithm='ball_tree').fit(extracted)
-    distances, indices = nbrs.kneighbors(extracted)
-    distances = np.sort(distances, axis=0)
-    distances = distances[:,1]    
-    knee_x = KneeLocator(range(1, len(distances)+1), distances, curve='convex', direction='increasing').knee
-    eps_ = distances[knee_x]    
-    clustering = DBSCAN(eps=eps_, min_samples=nn).fit(extracted)
-    score = silhouette_score(extracted, clustering.labels_, metric='euclidean')
-    print("nn: ", nn, " score ", score , " nc ", max(clustering.labels_))
-    scores[j] =score
-    
-#Adesso applico il clustering con il massimo score
-max_score = max(scores, key=scores.get)
-right_eps = scores[max_score]
-clustering = DBSCAN(eps=right_eps, min_samples=max_score*extracted.shape[1]).fit(extracted)
-score = silhouette_score(extracted, clustering.labels_, metric='euclidean')
-print("Score finale: ", score)
-extracted_dbscan = extracted.copy()
-extracted_dbscan["c"] = np.nan 
-extracted_dbscan["c"] = clustering.labels_
+#Clustering agglomerativo
+from sklearn.cluster import AgglomerativeClustering
+clustering = AgglomerativeClustering(n_clusters=6).fit(extracted)
+labels_agglomerative = clustering.labels_
+print("SC agg: ", silhouette_score(extracted, labels_agglomerative, metric='euclidean'))
+print("DB agg: ", davies_bouldin_score(extracted, labels_agglomerative))
 
-#ward poc
+
+
+#Birch
+from sklearn.cluster import Birch
+brc = Birch(n_clusters=6)
+brc.fit_predict(extracted)
+birch_labels = brc.labels_
+print("SC bir: ", silhouette_score(extracted, birch_labels, metric='euclidean'))
+print("DB bir: ", davies_bouldin_score(extracted, birch_labels))
+
+
+
+
+
+
+
 
 
 
